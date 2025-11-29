f4861c17c070d4487782e657c377b517c4f88c96
Patrick KAMDEM <patrick.kamdem@atos.net>
2025-11-29 15:38:22 +0100

    Add third subnet and tags

--- variables.tf
+++ variables.tf
@@ -26,6 +26,11 @@ variable "cidr_block" {
   default     = "10.42.0.0/16"
 }
 
+variable "billing_code" {
+  type        = string
+  description = "(Required) Billing code"
+}
+
 variable "public_subnets" {
   type        = map(string)
   description = "(Optional) Map of public subnets to create with CIDR blocks. Key will be used as subnet name with prefix. Default: {subnet-1 ="
