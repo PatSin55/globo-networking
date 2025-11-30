087944295a79c46cd8fd67d312a0634d4a668d51
Patrick KAMDEM <patrick.kamdem@atos.net>
2025-11-30 15:16:20 +0100

    Switch to using tfe_outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,14 @@
 ##################################################################################
 # OUTPUT
 ##################################################################################
+
+
+output "public_subnets" {
+  value       = module.main.public_subnets
+  description = "The different public subnets"
+}
+
+output "vpc_id" {
+  value       = module.main.vpc_id
+  description = "The id of the vpc"
+}
